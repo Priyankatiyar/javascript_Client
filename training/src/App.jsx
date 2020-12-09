6e86943b024bb87a98b61704653a2e5bbf7631cd
Priyankatiyar <priyanka.katiyar@successive.tech>
2020-12-09 16:05:47 +0530

    feat: Getting started with React(Component and Tools) #40385

---
+++ training/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { TextFieldDemo } from './pages';
+
+class App extends Component {
+  render() {
+    return (
+      <TextFieldDemo />
+    );
+  }
+}
+
+export default App;
---
+++ training/src/components/TextField/style.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+const Div = styled.div`2%;`;
+
+const Error = styled.p`color: red;`;
+
+const Input = styled.input`
+width: 95%;
+padding: 0.7%;
+border: 1px solid gray;
+border-radius: 5px;
+color: solid gray;
+
+${(props) => props.error
+&& css`
+border: 1px solid red;
+color: red;
+`};
+}
+
+${(props) => (props.value && !props.disabled && !props.error)
+&& css`
+border: 1px solid orange;
+color: black;
+`};
+}`;
+
+export { Div, Error, Input };
