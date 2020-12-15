4ebe7b07113a018dfccd0471e4c9d1fbeb93bf2a
Priyankatiyar <priyanka.katiyar@successive.tech>
2020-12-15 17:13:42 +0530

    feat: React Children and Material UI Setup #40791

---
+++ training/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Comic Sans MS',
+      'cursive',
+      'sans-serif',
+    ],
+    htmlFontSize: 10,
+  },
+});
+
+export default theme;
