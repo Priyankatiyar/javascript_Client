5bb33322bc23b3364d11711390b2fa045c928fe0
Priyankatiyar <priyanka.katiyar@successive.tech>
2021-01-06 18:14:27 +0530

    modify file

--- training/src/contexts/index.js
+++ training/src/contexts/index.js
@@ -1,5 +1,1 @@
 export { MyContext, SnackBarProvider } from './SnackBarProvider';
-
-// import { MyContext, SnackBarProvider } from './SnackBarProvider';
-
-// export { MyContext, SnackBarProvider };
